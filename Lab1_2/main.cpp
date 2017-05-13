0f2dfede53d15a78ffeba906039ab254540dbfe8
Igor_Turcevich <vip-turcevich@yandex.by>
2017-05-13 22:29:08 +0300

    Добавлены недостающие комментарии

--- Lab1_2/main.cpp
+++ Lab1_2/main.cpp
@@ -17,26 +17,27 @@ __fastcall TForm1::TForm1(TComponent* Owner)
 
 DoSmoke ATrain;
 
+// event handler when you click on button <Draw train>
 void __fastcall TForm1::Button1Click(TObject *Sender)
 {
 	ATrain.ShowTrain(Canvas);
 }
 //---------------------------------------------------------------------------
 
-
+// event handler when you click on button <Produce smoke>
 void __fastcall TForm1::Button2Click(TObject *Sender)
 {
 	Canvas->Brush->Color = clGray;
 	ATrain.ProduceSmoke(Canvas);
 }
 //---------------------------------------------------------------------------
-
+// event handler when you click on button <Exit>
 void __fastcall TForm1::Button4Click(TObject *Sender)
 {
     Form1->Close();
 }
 //---------------------------------------------------------------------------
-
+// event handler when you click on button <Remove smoke>
 void __fastcall TForm1::Button3Click(TObject *Sender)
 {
     ATrain.RemoveSmoke(Canvas, 120, 180);
