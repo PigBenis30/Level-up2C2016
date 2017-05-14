ff36a99aa03ef2638d6fee2a2e112f028ffa3959
Igor_Turcevich <vip-turcevich@yandex.by>
2017-05-14 07:16:40 +0300

    изменены названия переменных

--- Lab1_1/Ellipse.h
+++ Lab1_1/Ellipse.h
@@ -1,33 +1,33 @@
 #include <vcl.h>
  // class Ellipsñ
-class EllipseObject
+class Ellipse
 {
 	protected:
-	int x, y, a, b;
+	int x_, y_, aAxis_, bAxis_;
 
 	public:
 	// constructor without initialization parameters
-	EllipseObject() {}
+	Ellipse() {}
 	// func which draw ellipse on screen
-	void ShowEllipse(TCanvas *Canvas, int xe, int ye, int ae, int be)
+	void showEllipse(TCanvas *Canvas, int xe, int ye, int ae, int be)
 	 {
-		x = xe;
-		y = ye;
-		a = ae;
-		b = be;
+		x_ = xe;
+		y_ = ye;
+		aAxis_ = ae;
+		bAxis_ = be;
 		Canvas->Pen->Color=clRed;
-		Canvas->Ellipse(x - a, y - b, x + a, y + b);
+		Canvas->Ellipse(x_ - aAxis_, y_ - bAxis_, x_ + aAxis_, y_ + bAxis_);
 	 }
 	 // func which hide ellipse
-	 void HideEllipse(TCanvas *Canvas)
+	 void hideEllipse(TCanvas *Canvas)
 	 {
 		Canvas->Pen->Color=clWhite;
 		Canvas->Rectangle(0, 0, 600, 800);
 	 }
 	 // func which move ellipse on specified coordinates 
-	 void MoveEllipse(TCanvas *Canvas, int xn, int yn)
+	 void moveEllipse(TCanvas *Canvas, int xn, int yn)
 	 {
-		int xe = x, ye = y;
+		int xe = x_, ye = y_;
 		bool isAvaible = true;
 
 		while(isAvaible) {
@@ -49,42 +49,42 @@ class EllipseObject
 				isAvaible = true;
 			}
 
-			HideEllipse(Canvas);
-			x = xe;
-			y = ye;
-			ShowEllipse(Canvas, x, y, a, b);
+			hideEllipse(Canvas);
+			x_ = xe;
+			y_ = ye;
+			showEllipse(Canvas, x_, y_, aAxis_, bAxis_);
 			Sleep(10);
 		}
 	 }
-	 // func fich 
-	 void ScaleEllipse(TCanvas *Canvas, int xn, bool isIncrease)
+	 // func which scale ellipse 
+	 void scaleEllipse(TCanvas *Canvas, int xn, bool isIncrease)
 	 {
-		for (int Count = 1; Count<=xn; Count++){
-			HideEllipse(Canvas);
+		for (int Count = 1; Count <= xn; Count++){
+			hideEllipse(Canvas);
 			if (isIncrease)
-				ShowEllipse(Canvas, x, y, a + 1, b + 1);
+				ShowEllipse(Canvas, x_, y_, aAxis_ + 1, bAxis_ + 1);
 			else
-				ShowEllipse(Canvas, x, y, a - 1, b - 1);
+				ShowEllipse(Canvas, x_, y_, aAxis_ - 1, bAxis_ - 1);
 			Sleep(30);
-			if (a <= 0 || b <= 0){
-				ShowMessage("Ôèãóðà èñïàðèëàñü");
+			if (aAxis_ <= 0 || bAxis_ <= 0){
+				ShowMessage("Фигура испарилась");
 				return;
 			}
 		}
 	 }
 	 // func which find ellipse area
-	 int AreaEllipse()
+	 int computeArea()
 	 {
-		 return a * b * M_PI;
+		 return aAxis_ * bAxis_ * M_PI;
 	 }
 	 // func which find ellipse perimetr
-	 int PerimetrEllipse()
+	 int computePerimetr()
 	 {
-		if ((a+b) == 0)
+		if (( aAxis_ + bAxis_) == 0)
 			return 0;
 		else
-			return 4 * (M_PI * a * b + (a - b)) / (a + b);
+			return 4 * (M_PI * aAxis_ * bAxis_ + (aAxis_ - bAxis_)) / (aAxis_ + bAxis_);
 	 }
 	 // destructor
-	 ~EllipseObject(){}
+	 ~Ellipse(){}
 };
